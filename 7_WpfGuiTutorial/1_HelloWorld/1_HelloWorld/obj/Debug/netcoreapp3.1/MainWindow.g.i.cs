7631137775601e22d646283dc69b5f1ad7e42c38
hamidreza.abooei <hamidreza.abooei@gmail.com>
2021-05-20 11:57:21 +0430

    UI design

--- 7_WpfGuiTutorial/1_HelloWorld/1_HelloWorld/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ 7_WpfGuiTutorial/1_HelloWorld/1_HelloWorld/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -165,7 +165,7 @@ namespace _1_HelloWorld {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/1_HelloWorld;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/1_HelloWorld;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
