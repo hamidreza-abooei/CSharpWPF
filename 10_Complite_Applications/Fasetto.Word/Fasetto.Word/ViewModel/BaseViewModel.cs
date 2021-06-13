69ef85f7cfd3c1fe440a471ac506f56294a0a4a3
hamidreza.abooei <hamidreza.abooei@gmail.com>
2021-06-13 22:36:07 +0430

    [Add] New Project

---
+++ 10_Complite_Applications/Fasetto.Word/Fasetto.Word/ViewModel/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.ComponentModel;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// A base View Model that fires property changed events as needed 
+    /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// the event that is fired with any child property changes its value
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => {};
+    }
+
+}
